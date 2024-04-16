6be38d2a9d3ff6af4282eebbba398200a15bb11f
Ahnaf-dev <ahnafbari99@gmail.com>
2024-04-16 13:52:54 -0600

    Update

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   const fetchServer = async () => {
-    let response = await fetch("https://fullstack-deployment-test.vercel.app/");
+    let response = await fetch("https://deploy-fullstack-test-server.vercel.app/");
     console.log(response)
   }
 
